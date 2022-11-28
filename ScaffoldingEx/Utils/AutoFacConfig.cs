160b38a64cc16019dd1a6aa7b18120dccc7b4578
kim0111 <86676168+kim0111@users.noreply.github.com>
2022-11-28 19:45:32 +0600

    Add files via upload

---
+++ ScaffoldingEx/Utils/AutoFacConfig.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using ScaffoldingEx.Models;
+using ScaffoldingEx.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
+namespace ScaffoldingEx.Utils
+{
+    public class AutoFacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            /*builder.RegisterType<HomeRepository>().As<IHomeRepository>().WithParameter("dbcontext",new MyMusicDBEntities());*/
+            builder.RegisterType<HomeRepository>().As<IHomeRepository>().WithParameters(new List<Parameter> { new NamedParameter("dbcontext", new ModelAvenContext()), new NamedParameter("conString", "vugbhdinjoa") });
+            builder.RegisterType<HomeRepository>().As<IHomeRepository>().WithProperty("dbcontext", new ModelAvenContext());
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
---
+++ ScaffoldingEx/Utils/NinjectRegu.cs
@@ -0,0 +1,21 @@
+﻿using Ninject.Modules;
+using ScaffoldingEx.Models;
+using ScaffoldingEx.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+
+namespace ScaffoldingEx.Utils
+{
+    public class NinjectRegu : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IHomeRepository>().To<HomeRepository>().WithConstructorArgument("dbEntities", new ModelAvenContext());
+        }
+
+    }
+}
